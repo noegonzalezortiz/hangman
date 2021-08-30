a09a3c82b34c212c5190ca8b377b57c624bc03ff
Noe_G <noe.gonzalezortiz@gmail.com>
2021-08-30 14:57:55 -0400

    fixed variable naming and missing elements

--- index.js
+++ index.js
@@ -2,106 +2,119 @@ const prompt = require("readline-sync");
 const wordBank = require("./word-bank.json");
 
 
+let win = 0;
+let loss = 0;
+
+
+//default variables to reset the game
 const reset = () => {
   let answerArray = [];
-  let guessNum = 6;
-  let lettersGuessed = [];
+  let guesses = 6;
+  let guessedLetters = [];
+  
 
+  //main game function, selects a random word from the word bank and splits it into an array of letters
   const game = () => {
     const word = wordBank[Math.floor(Math.random() * wordBank.length)];
     const letters = word.split("");
     let remainingLetters = word.length;
-
-    const findAnswerArr = () => {
+  
+    const findAnswerArray = () => {
       letters.forEach((_, index) => {
-        answerArray[index] = "_";
+        answerArray[index] = "_"
       });
     };
-  
-    findAnswerArr();
-
-    console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
-      
     
-    while (remainingLetters > 0 && guessNum > 0) {
+    findAnswerArray();
+    
+    console.log("\nWelcome to Hangman!\nPress ctrl+c to stop\n");
+    
+    while (remainingLetters > 0 && guesses > 0) {
       console.log(answerArray.join(" "));
-      let guess = prompt.question("Please guess a letter: ").toLowerCase;
-                        
+      let guess = prompt.question("\nPlease guess a letter: ").toLowerCase();
+
+
+      //check the guess to make sure that it is a string and/ or part of the word      
+      
       const checkGuess = () => {
         if (guess === "") {
           console.log("\nPlease enter a letter.")
         } else if (/[^a-zA-Z]/.test(guess[0])) {
           console.log("\nPlease enter a letter.")
         } else if (/[a-zA-Z]/.test(guess[0])) {
-          letters.forEach((letter, i) => {
+          letters.forEach((letter, index) => {
             if (guess[0] === letter) {
-              if (answerArray[i] === "_") {
-                answerArray[i] = guess[0];
+              if (answerArray[index] === "_") {
+                answerArray[index] = guess[0];
                 remainingLetters--;
               };
             };
           });
-          if (!lettersGuessed.includes(guess[0]) && !letters.includes(guess[0])) {
-            guessNum-- 
+          if (!guessedLetters.includes(guess[0]) && !letters.includes(guess[0])) {
+            guesses--
           };
-          if (!lettersGuessed.includes(guess[0])) {
-            lettersGuessed.push(guess[0]);};
-            };
+          if (!guessedLetters.includes(guess[0])) {
+            guessedLetters.push(guess[0]);
           };
         };
-
-    checkGuess();
-     
-    const drawHangman = () => {
-      if (guessNum === 6) {
-        console.log(`\nYou have ${guessNum} guesses left.`)
-      } else if (guessNum === 5) {
-        console.log(`\n O \n\n\n`);
-        console.log(`You have ${guessNum} guesses left.`)
-      } else if (guessNum === 4) {
-        console.log(`\n O \n | \n\n`)
-        console.log(`You have ${guessNum} guesses left.`)
-      } else if (guessNum === 3) {
-        console.log(`\n O \n/| \n\n`)
-        console.log(`You have ${guessNum} guesses left.`)
-      } else if (guessNum === 2) {
-        console.log(`\n O \n/|\\\n\n`)
-        console.log(`You have ${guessNum} guesses left.`)
-      } else if (guessNum === 1) {
-        console.log(`\n O \n/|\\\n/\n`)
-        console.log(`You have ${guessNum} guesses left.`)
-      } else if (guessNum === 0) {
-        console.log(`\n O \n/|\\\n/ \\\n`)
-        console.log(`You have ${guessNum} guesses left.`)
       };
-      console.log(`You have guessed ${lettersGuessed}.\n`);
+
+      checkGuess();
+
+      //draws stick figure in terminal, contigent to the amount of guesses left
+      const drawHangman = () => {
+        if (guesses === 6) {
+          console.log(`\nYou have ${guesses} guesses left.`)
+        } else if (guesses === 5) {
+          console.log(`\n O \n\n\n`);
+          console.log(`You have ${guesses} guesses left.`)
+        } else if (guesses === 4) {
+          console.log(`\n O \n | \n\n`)
+          console.log(`You have ${guesses} guesses left.`)
+        } else if (guesses === 3) {
+          console.log(`\n O \n/| \n\n`)
+          console.log(`You have ${guesses} guesses left.`)
+        } else if (guesses === 2) {
+          console.log(`\n O \n/|\\\n\n`)
+          console.log(`You have ${guesses} guesses left.`)
+        } else if (guesses === 1) {
+          console.log(`\n O \n/|\\\n/\n`)
+          console.log(`You have ${guesses} guesses left.`)
+        } else if (guesses === 0) {
+          console.log(`\n O \n/|\\\n/ \\\n`)
+          console.log(`You have ${guesses} guesses left.`)
+        };
+        console.log(`You have guessed ${guessedLetters}.\n`);
+      };
+
+      drawHangman();
+    
     };
 
-    drawHangman();
-
-    };
-
-      const showResults = () => {
+    //result promts, joins the answer array letters to form a single string
+    const showResults = () => {
       if (remainingLetters > 0) {
         console.log(answerArray.join(" "));
         console.log(`\n\nSorry, you are out of guesses.\nThe answer was ${word}.\n`)
-        losses++;
+        loss++;
       } else {
         console.log(answerArray.join(" "));
         console.log(`\n\nGood job!\nThe answer was ${word}.\n`)  
-        wins++;
+        win++;
       };
-      console.log(`\nYou won ${wins} rounds and have lost ${losses} rounds.\n`)
+      console.log(`\nYou have won ${win} round(s) and have lost ${loss} round(s).\n`)
     };
-
+    
     showResults();
-
+  
   };
- game();
+  
+  game();
 
+};
 
-
+//resets the code to be able to play multiple rounds
 
 while(true) {
-    reset();
+  reset();
 };
